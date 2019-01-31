0cc8767abac7f680299949c7e55717a5cf1369a4
metafield <anthony.sylvester99@gmail.com>
2019-01-30 17:03:59 -0800

    basic sign in

---
+++ src/components/session/SignUp.js
@@ -0,0 +1,39 @@
+import firebase from 'firebase'
+import React from 'react';
+
+
+class SignUp extends React.Component {
+    state = {}
+
+    onSignUp = async (e) => {
+        e.preventDefault()
+        const { email, password } = this.state
+
+        try {
+            const response = await firebase.auth().createUserWithEmailAndPassword(email, password)
+            console.log(response);
+        } catch (error) {
+            console.error(error.message);
+            this.setState({ error: error.message })
+        }
+    }
+
+    onInput = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+        const { error } = this.state
+        return <React.Fragment>
+            <form>
+                <h2>Sign up</h2>
+                <input type="email" name="email" onInput={this.onInput} /> <br />
+                <input type="password" name="password" onInput={this.onInput} /> <br />
+                <input type="submit" value="Sign Up" onClick={this.onSignUp}/>
+                {error && <h4 style={{color: 'red'}}>{this.state.error}</h4>}
+            </form>
+        </React.Fragment>
+    }
+}
+
+export default SignUp
