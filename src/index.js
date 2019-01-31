f3efd266fc6a73cedfef50818c4b1140a5e75cf9
Anthony Sylvester <anthony.sylvester99@gmail.com>
2019-01-31 10:03:04 -0800

    cleans up to use as a boilerplate

--- src/index.js
+++ src/index.js
@@ -5,17 +5,9 @@ import App from './App';
 import firebase from 'firebase'
 import * as serviceWorker from './serviceWorker';
 
-const config = {
-    apiKey: "AIzaSyDCeDsz6vE_rAfN-14QLiyvGx4p7MocYVw",
-    authDomain: "redshift-fps.firebaseapp.com",
-    databaseURL: "https://redshift-fps.firebaseio.com",
-    projectId: "redshift-fps",
-    storageBucket: "redshift-fps.appspot.com",
-    messagingSenderId: "228074821025"
-  };
+const config = {};
   
 firebase.initializeApp(config);
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
