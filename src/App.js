da81b19dcd9deedd545d866b6ab26ffe4c2b4d60
metafield <anthony.sylvester99@gmail.com>
2019-01-30 17:11:39 -0800

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import SignUp from './components/session/SignUp';
 
@@ -6,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+            </Switch>
+        </Router>
         <header className="App-header">
           <SignUp />
         </header>
